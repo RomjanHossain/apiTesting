a723c3dfeb03b4a4c00c047c0a39f457108e82e7
RomjanHossain <romjanhossain726526@gmail.com>
2020-05-11 17:22:30 +0600

    Updates

---
+++ precache-manifest.a41fa2071dce22a6de325a18d2585bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c224cdd489e658359a25465e591f35",
+    "url": "/apiTesting/index.html"
+  },
+  {
+    "revision": "5b71cc57fafa66712aef",
+    "url": "/apiTesting/static/css/main.40d3b6c1.chunk.css"
+  },
+  {
+    "revision": "707bb4dd4f5637da0c1a",
+    "url": "/apiTesting/static/js/2.65df30c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apiTesting/static/js/2.65df30c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b71cc57fafa66712aef",
+    "url": "/apiTesting/static/js/main.5b0e551a.chunk.js"
+  },
+  {
+    "revision": "9905c8a6cd1503b207b9",
+    "url": "/apiTesting/static/js/runtime-main.810ac11d.js"
+  }
+]);
