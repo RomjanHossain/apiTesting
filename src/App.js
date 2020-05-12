6829943a9f79c90542fe42934ba6880acb3e54c2
RomjanHossain <romjanhossain726526@gmail.com>
2020-05-12 17:34:19 +0600

    compo add and render done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './index.css';
+import {SearchBox} from './components/searchBox/search.components';
+import {CardList} from './components/card-list/card-list.components';
 
-function App() {
-  return (
-    <div>
-        <h1>Welcome</h1>
-    </div>
-  );
+class App extends React.Component{
+    constructor(){
+        super ();
+        this.state = {
+            result: [],
+            searchField:''
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://randomuser.me/api/?results=100"
+).then(
+            response => response.json()
+        ).then(results=> this.setState({result:results.results}));
+    }
+    handleSearch = (e) => {
+        this.setState(
+            { searchField: e.target.value }
+        )
+    }
+    render(){
+        const { result, searchField } = this.state
+        const filterSearched = result.filter(names => (names.name.first+names.name.last).toLowerCase().includes(searchField.toLowerCase()))
+        return (
+            <div>
+                <h1>“모든 사람은 컴퓨터가 이해할 수있는 코드를 작성할 수 있습니다.</h1>
+                {
+                <SearchBox placeholder='search Names' handleSearch={this.handleSearch}/>
+    }{<CardList data={filterSearched}/>}
+            </div>
+        )
+    }
 }
 
 export default App;
